91c3b48db1bc782163b3790bd90355fc37f6ae9c
GeorgiPanayotov <134947988+GeorgiPanayotov@users.noreply.github.com>
2025-01-12 20:33:33 +0200

    Add: accounts & recipes apps added to the structure, both registered in Installed Apps

--- FlavorDiary/settings.py
+++ FlavorDiary/settings.py
@@ -28,7 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
+MY_APPS = [
+    'recipes',
+    'accounts'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -74,9 +77,13 @@ WSGI_APPLICATION = 'FlavorDiary.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "flavordiary",
+        "USER": "macbook",
+        "PASSWORD": "270395",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
